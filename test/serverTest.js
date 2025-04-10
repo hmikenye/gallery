113dab2a6ed8dc0f91044f7e4d5780a01d388278
hmikenye <hmikenye@gmail.com>
2025-04-10 14:07:47 +0300

    Add new changes

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,21 +1,27 @@
+// test/server.test.js
 process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../server'); // this should be the exported express app
+const app = require('../server'); // Express app, not app.listen()
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-it('should get all photos', function(done) {
-  chai.request(app)
-    .get('/')
-    .end(function(err, res) {
-      if (err) return done(err);
-      expect(res).to.have.status(200);
-      expect(res.body).to.be.an('array');
-      done();
-    });
+describe('Photos API', function() {
+  it('should GET all photos', function(done) {
+    chai.request(app)
+      .get('/photos')
+      .end(function(err, res) {
+        if (err) return done(err);
+
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body.length).to.be.above(0);
+        expect(res.body[0]).to.have.property('id');
+        expect(res.body[0]).to.have.property('url');
+
+        done();
+      });
+  });
 });
-
-
