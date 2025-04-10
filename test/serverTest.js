14748869fc2b0a22dc026663ee45b78bb5e6543f
hmikenye <hmikenye@gmail.com>
2025-04-10 15:04:55 +0300

    Add servertestjs

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,27 +1,24 @@
-// test/server.test.js
-process.env.NODE_ENV = 'test';
-
 const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../server'); // Express app, not app.listen()
-const expect = chai.expect;
-
-chai.use(chaiHttp);
-
-describe('Photos API', function() {
-  it('should GET all photos', function(done) {
-    chai.request(app)
-      .get('/photos')
-      .end(function(err, res) {
-        if (err) return done(err);
-
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.an('array');
-        expect(res.body.length).to.be.above(0);
-        expect(res.body[0]).to.have.property('id');
-        expect(res.body[0]).to.have.property('url');
-
-        done();
-      });
-  });
-});
+ const chaiHttp = require('chai-http');
+ const app = require('../server'); // ✅ this now only contains the app
+ const app = require('../server'); // this should be the exported express app
+ const expect = chai.expect;
+ 
+ chai.use(chaiHttp);
+ 
+ describe('Photos', function () {
+   it('should GET all photos', function (done) {
+ describe('Photos', function() {
+   it('should get all photos', function(done) {
+     chai.request(app)
+       .get('/') // make sure this route returns photos or adjust accordingly
+       .end(function (err, res) {
+       .get('/')
+       .end(function(err, res) {
+         expect(res).to.have.status(200);
+         expect(res.body).to.be.an('array'); // depends on your actual response
+         expect(res.body).to.be.an('array'); // adjust depending on your response
+         done();
+       });
+   });
+ });
