18d5abc8076285c607266ff25e3860839e3b3e6c
hmikenye <hmikenye@gmail.com>
2025-04-10 15:20:16 +0300

    Add new change

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,23 +1,20 @@
 const chai = require('chai');
- const chaiHttp = require('chai-http');
- const app = require('../server'); // this should be the exported express app
- const expect = chai.expect;
- 
- chai.use(chaiHttp);
- 
- describe('Photos', function () {
-   it('should GET all photos', function (done) {
- describe('Photos', function() {
-   it('should get all photos', function(done) {
-     chai.request(app)
-       .get('/') // make sure this route returns photos or adjust accordingly
-       .end(function (err, res) {
-       .get('/')
-       .end(function(err, res) {
-         expect(res).to.have.status(200);
-         expect(res.body).to.be.an('array'); // depends on your actual response
-         expect(res.body).to.be.an('array'); // adjust depending on your response
-         done();
-       });
-   });
- });
+const chaiHttp = require('chai-http');
+const app = require('../server'); // this should be the exported Express app
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Photos', function () {
+  it('should GET all photos', function (done) {
+    chai.request(app)
+      .get('/photos') // make sure this matches your actual route in server.js
+      .end(function (err, res) {
+        if (err) return done(err); // handle error if test fails
+
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('array');
+        done();
+      });
+  });
+});
