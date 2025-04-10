fbd8a0aa38d44c3d4025bb63abe0861079267470
hmikenye <hmikenye@gmail.com>
2025-04-10 14:00:54 +0300

    Add new test

--- test/serverTest.js
+++ test/serverTest.js
@@ -7,15 +7,15 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('Photos', function() {
-  it('should get all photos', function(done) {
-    chai.request(app)
-      .get('/')
-      .end(function(err, res) {
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.an('array'); // adjust depending on your response
-        done();
-      });
-  });
+it('should get all photos', function(done) {
+  chai.request(app)
+    .get('/')
+    .end(function(err, res) {
+      if (err) return done(err);
+      expect(res).to.have.status(200);
+      expect(res.body).to.be.an('array');
+      done();
+    });
 });
+
 
